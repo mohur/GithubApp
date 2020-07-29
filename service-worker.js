fd9955169f4aea5afffa0f4a5a46513755cd5d04
mohur <m.ashrahfi@gmail.com>
2020-07-29 10:02:54 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/GithubApp/precache-manifest.5562c29717d5eb25d91034544cff9529.js"
+  "/GithubApp/precache-manifest.6ba3b381e2aac5e6663868d225b7f7b8.js"
 );
 
 self.addEventListener('message', (event) => {
